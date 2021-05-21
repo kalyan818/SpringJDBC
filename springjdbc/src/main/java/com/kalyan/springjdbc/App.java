6b2262d9d8fa38411e3eba71bd64f379e6692626
Kalyan sai <kalyansaihnk123@gmail.com>
2021-05-21 13:38:30 +0530

    Add files via upload

---
+++ springjdbc/src/main/java/com/kalyan/springjdbc/App.java
@@ -0,0 +1,23 @@
+package com.kalyan.springjdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate =  (JdbcTemplate)context.getBean("jdbcTemplate");
+        String sql = "insert into employee values(?,?,?)";
+        int result = jdbcTemplate.update(sql,10,"kalyan","ratan");
+        System.out.println("Number of records inserted are:"+result);
+       
+    }
+}
